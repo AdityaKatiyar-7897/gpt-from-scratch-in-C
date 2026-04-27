96e3553384ab4590a294fdf77e1d9dd3db0b0785
Aditya Katiyar <rishkat1249@gmail.com>
2026-04-27 19:04:50 +0530

    Update dataloader.c

--- src/dataloader.c
+++ src/dataloader.c
@@ -6,34 +6,34 @@
 int main() {
     long size;
 
-    // 1. Open the file
+    // Open the file
     FILE* file = fopen("data/shakespeare.txt", "rb");
 
-    // 2. Move cursor to end
+    // Move cursor to end
     fseek(file, 0, SEEK_END);
 
-    // 3. Get the size
+    // Get the size
     size = ftell(file);
 
-    // 4. Move cursor back to start
+    // Move cursor back to start
     fseek(file, 0, SEEK_SET);
 
-    // 5. Print the size
+    // Print the size
     printf("File size: %ld bytes\n", size);
 
-    // 6. Allocate memory for the text
+    // Allocate memory for the text
     char* text = malloc(size + 1);
 
     // 7. Read the file into memory
     fread(text, 1, size, file);
 
-    // First: walk the text and mark seen characters
+    // walk the text and mark seen characters
     int seen[256] = {0};
     for (int i = 0; i < size; i++) {
         seen[(unsigned char)text[i]] = 1;
     }
     
-    // Second: print which characters were seen
+    // print which characters were seen
     for (int i = 0; i < 256; i++) {
         if (seen[i] == 1) {
             printf("char: '%c' = %d\n", i, i);
@@ -101,16 +101,16 @@ int main() {
 
     
     
-    // 8. Null terminate it
+    // Null terminate it
     text[size] = '\0';
 
-    // 9. Print first 100 characters to verify
+    // Print first 100 characters to verify
     printf("First 100 chars:\n%.100s\n", text);
 
-    // 10. Free the memory
+    // Free the memory
     free(text);
 
-    // 11. Close the file
+    // Close the file
     fclose(file);
 
     return 0;
